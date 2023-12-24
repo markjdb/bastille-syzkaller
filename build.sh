44b84cf0e1f3bb6f333adb998dae1266a4c0d8d6
Mark Johnston <markj@FreeBSD.org>
2023-12-24 13:26:37 -0500

    Add ZFS support

--- build.sh
+++ build.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
+usage()
+{
+    echo "usage: $0 [-a] [-t ufs|zfs]" >&2
+    exit 1
+}
+
 set -e
 
 KERNFAST=-DKERNFAST
+: ${VMFS:=ufs}
 
-while getopts a o; do
+while getopts at: o; do
     case "$o" in
     a)
         KERNFAST=
         ;;
+    t)
+        VMFS=$OPTARG
+        if [ "$VMFS" != "ufs" -a "$VMFS" != "zfs" ]; then
+            usage
+        fi
+        ;;
     *)
-        exit 1
+        usage
         ;;
     esac
     shift $((OPTIND - 1))
@@ -24,9 +37,18 @@ make -s -j$(sysctl -n hw.ncpu) installkernel \
     KERNCONF=SYZKALLER KERNCONFDIR=/root DESTDIR=${_FREEBSD_BUILDROOT} \
     SRCCONF=${_FREEBSD_BUILD_SRCCONF}
 
-makefs -B little -M 10g -S 512 -Z -o label=VM -o softupdates=1 -o version=2 \
-    /root/vm.part ${_FREEBSD_BUILDROOT}
+if [ $VMFS = ufs ]; then
+    parttype=freebsd-ufs
+    makefs -B little -M 10g -S 512 -Z -o label=VM -o softupdates=1 -o version=2 \
+        /root/vm.part ${_FREEBSD_BUILDROOT}
+else
+    parttype=freebsd-zfs
+    makefs -s 15g -t zfs -o poolname=zroot -o bootfs=zroot -o rootpath=/ \
+        /root/vm.part ${_FREEBSD_BUILDROOT}
+    echo "gpt/rootfs / ufs rw 0 0" > ${_FREEBSD_BUILDROOT}/etc/fstab
+fi
+
 mkimg -s gpt -f raw -S 512 -b ${_FREEBSD_BUILDROOT}/boot/pmbr \
     -p freebsd-boot/bootfs:=${_FREEBSD_BUILDROOT}/boot/gptboot \
-    -p freebsd-ufs/rootfs:=/root/vm.part \
+    -p ${parttype}/rootfs:=/root/vm.part \
     -o ${_SYZKALLER_VM_IMG}
